41e95f878215401f28fb547293037d0fafcd263d
Arturo Vivar <arthas1237@hotmail.com>
2022-06-09 20:38:35 +0200

    text animated

--- src/components/Main/Title.jsx
+++ src/components/Main/Title.jsx
@@ -3,6 +3,9 @@ import './Title.css'
 
 export default function Title() {
   return (
-    <div className='title'><h1><a>WORK IN PROGRESS</a></h1></div>
+      <div className='wrapper title'>
+    <div className='line__1'><h1><a>WORK IN</a></h1></div>
+    <div className='line__2'><h1><a>PROGRESS</a></h1></div>
+    </div>
   )
 }
