44fa76386d8e4015bc44daef46844704bdab5efe
Arturo Vivar <arthas1237@hotmail.com>
2022-06-14 16:25:37 +0200

    added title

--- src/components/Main/Title.jsx
+++ src/components/Main/Title.jsx
@@ -4,8 +4,9 @@ import './Title.css'
 export default function Title() {
   return (
       <div className='wrapper title'>
-    <div className='line__1'><h1><a>WORK IN</a></h1></div>
-    <div className='line__2'><h1><a>PROGRESS</a></h1></div>
+        <div><h1><a>CYBERPVNKERS</a></h1></div>
+    <div className='line__1'><h3><a>WORK IN</a></h3></div>
+    <div className='line__2'><h3><a>PROGRESS</a></h3></div>
     </div>
   )
 }
